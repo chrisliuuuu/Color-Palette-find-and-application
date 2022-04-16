636a33a66beb529c13efa53ba599871493a8b304
KrishnaKothandaraman <kkrishna0701@gmail.com>
2022-04-16 19:16:44 -0400

    Boilerplate code for training

--- main.py
+++ main.py
@@ -1,27 +1,49 @@
+#!/usr/bin/env python3
+
 import sys
 
 from PIL import Image
 import numpy as np
 import os
 from typing import List, Tuple, Dict
+import logging
+import argparse
+import pathlib
 
 
 class TrainingData:
     temperature: float
+    training_file: str
+    confidence_level: float
 
-    def __init__(self):
+    def __init__(self, training_file: pathlib.Path, confidence_level: float):
+        self.temperature = -1
+        self.training_file = training_file.as_posix()
+        self.confidence_level = confidence_level
+
+    def train(self):
         self.temperature = -1
 
-    def train_from_folder(self, image_directory="./sample-image"):
-        self.temperature = -1
-        if not os.path.isdir(image_directory):
+        if not os.path.isdir(self.training_file):
             print("Image Root folder does not exist")
             sys.exit(1)
 
-        for filename in os.listdir(image_directory):
-            if not filename.endswith(".jpeg"):
+        for filename in os.listdir(self.training_file):
+            if not filename.endswith(".jpg"):
                 continue
 
+            logging.info(f"Training with: {filename}")
+
+
 if __name__ == "__main__":
-    t = TrainingData()
-    t.train_from_folder()
+    p = argparse.ArgumentParser(description="Image Processor")
+    p.add_argument('-f', dest="training_file", type=pathlib.Path, help="Location of directory containing training "
+                                                                       "images", required=True)
+    p.add_argument('-c', dest="confidence", type=float, help="Minimum confidence level required to use a colour",
+                   default=1)
+    args = p.parse_args()
+    logging.basicConfig(level=logging.INFO)
+
+    logging.info("Beginning training")
+    t = TrainingData(args.training_file, args.confidence)
+    t.train()
